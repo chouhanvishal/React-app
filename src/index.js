10831da54fe1400e0d2fe7d32fd15125b597d87e
chouhanvishal <chouhanvishal273@gmail.com>
2022-11-06 17:39:50 +0530

    use phases of life cycle of components

--- src/index.js
+++ src/index.js
@@ -11,17 +11,37 @@
 
 // reportWebVitals();
 
-
-import React, { createElement } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Car from './Car.js';
-import App from './App.js'
 
-
-const test= 'test'
-const myElement = React.createElement('marquee', {test}, 'I do not use JSX!');
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({favoritecolor: "yellow"})
+    }, 1000)
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    document.getElementById("div1").innerHTML =
+    "Before the update, the favorite was " + prevState.favoritecolor;
+  }
+  componentDidUpdate() {
+    document.getElementById("div2").innerHTML =
+    "The updated favorite is " + this.state.favoritecolor;
+  }
+  render() {
+    return (
+      <div>
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+      <div id="div1">dsfghj</div>
+      <div id="div2">dfghj</div>
+      </div>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    myElement
-);
+root.render(<Header />);
